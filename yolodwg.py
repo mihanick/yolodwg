c26d8846eb834536a49cbc8922771bbdcc5a4795
mihanick <mihanick@gmail.com>
2021-10-15 13:43:24 +0600

    Adapt resnet to generate pnt classes

--- yolodwg.py
+++ yolodwg.py
@@ -230,14 +230,18 @@ class DwgKeyPointsResNet50(nn.Module):
     '''
     https://debuggercafe.com/advanced-facial-keypoint-detection-with-pytorch/
     '''
-    def __init__(self, requires_grad=True, pretrained=True, max_points=100, num_coordinates=2, num_channels=3):
+    def __init__(self, requires_grad=True, pretrained=True, max_points=100, num_coordinates=2, num_pnt_classes=3, num_img_channels=3):
         super(DwgKeyPointsResNet50, self).__init__()
         self.max_points = max_points
         self.num_coordinates = num_coordinates
-        self.max_coords = self.max_points * self.num_coordinates
+        self.num_pnt_classes = num_pnt_classes
+        self.num_features = num_coordinates + num_pnt_classes + 1 # x, y, and each pnt cls and pnt_cls==0
+        self.output_size = self.max_points * self.num_features
+        self.num_channels = num_img_channels
+        
 
         self.model = models.resnet50(pretrained=pretrained)
-        self.model.fc = nn.Linear(2048, self.max_coords * 4)
+        self.model.fc = nn.Linear(2048, self.output_size * 4)
 
         if requires_grad == True:
             for param in self.model.parameters():
@@ -248,22 +252,17 @@ class DwgKeyPointsResNet50(nn.Module):
         for param in self.model.fc.parameters():
             param.requires_grad = True
 
-        self.bn = nn.BatchNorm1d(1000)
+        self.bn = nn.BatchNorm1d(1024)
         self.do = nn.Dropout()
         # add the final layer
-        self.l0 = nn.Linear(100, self.max_coords)
+        self.l0 = nn.Linear(100, self.output_size)
 
     def forward(self, x):
         # get the batch size only, ignore (c, h, w)
         batch, _, _, _ = x.shape
         x = self.model(x)
         x = x.reshape(batch, self.max_points, -1)
-        x = F.adaptive_max_pool2d(x, (self.max_points, 2))
-        x = x.reshape(batch, -1)
-        
-        # x = self.bn(x)
-        # x = self.l0(x)
-        # x = self.do(x)
+        x = F.adaptive_max_pool2d(x, (self.max_points, self.num_features))
         return x
 #------------------------------
 class DwgKeyPointsModel(nn.Module):
@@ -559,7 +558,8 @@ def run(
     val_loader   = dwg_dataset.val_loader
 
     # create model
-    model = DwgKeyPointsModel(max_points=dwg_dataset.entities.max_labels, num_pnt_classes=dwg_dataset.entities.num_pnt_classes, num_coordinates=dwg_dataset.entities.num_coordinates, num_img_channels=dwg_dataset.entities.num_image_channels)
+    #model = DwgKeyPointsModel(max_points=dwg_dataset.entities.max_labels, num_pnt_classes=dwg_dataset.entities.num_pnt_classes, num_coordinates=dwg_dataset.entities.num_coordinates, num_img_channels=dwg_dataset.entities.num_image_channels)
+    model = DwgKeyPointsResNet50(pretrained=True, requires_grad=True, max_points=dwg_dataset.entities.max_labels, num_pnt_classes=dwg_dataset.entities.num_pnt_classes, num_coordinates=dwg_dataset.entities.num_coordinates, num_img_channels=dwg_dataset.entities.num_image_channels)
     #model = DwgKeyPointsResNet50(requires_grad=True, pretrained=True, max_points=dwg_dataset.entities.max_labels, num_coordinates=dwg_dataset.entities.num_coordinates, num_channels=dwg_dataset.entities.num_image_channels)
     model.to(config.device)
 
@@ -660,4 +660,5 @@ if __name__ == "__main__":
         epochs=opt['epochs'],
         checkpoint_interval=opt['checkpoint_interval'],
         lr=opt['lr'],
+        validate=True,
         limit_number_of_records=opt['limit_number_of_records'])
