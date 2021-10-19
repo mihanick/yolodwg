6af31359af5abb41d7185054a84ea32fbc2636ad
mihanick <mihanick@gmail.com>
2021-10-19 10:58:03 +0600

    Add yolo4 model from https://github.com/Tianxiaomo/pytorch-YOLOv4

--- models/DwgKeyPointsModel.py
+++ models/DwgKeyPointsModel.py
@@ -49,3 +49,44 @@ class DwgKeyPointsModel(nn.Module):
         # x[:, :, 2:] = F.softmax(x[:, :, 2:])
 
         return x
+
+def test_model_input_plot():
+    import config
+    import torch
+    from yolodwg import DwgKeyPointsModel, DwgKeyPointsResNet50, non_zero_loss
+    from plot import plot_batch_grid
+
+    #model = DwgKeyPointsModel(max_points=max_points, num_coordinates=num_coordinates).to(config.device)
+
+    checkpoint = torch.load('runs/2/best.weights', map_location=config.device)
+    max_points = checkpoint['max_points']
+    num_pnt_classes = checkpoint['num_pnt_classes']
+
+    model = DwgKeyPointsModel(max_points=max_points, num_pnt_classes=num_pnt_classes)
+    #model = DwgKeyPointsResNet50(requires_grad=True, max_points=max_points)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.to(config.device)
+    model.eval()
+
+    res = None
+
+    for i in range(100):
+        imgs = torch.rand([4, 3, 128, 128])
+        imgs = imgs.to(config.device)
+        out = model(imgs)
+        if res is None:
+            res = out
+        else:
+            res = torch.cat([res, out])
+        #plot_batch_grid(
+        #        input_images=imgs,
+        #        true_keypoints=None,
+        #        predictions=out,
+        #        plot_save_file=f'runs/debug_{i}.png')
+
+    print(torch.min(res).item())
+    print(torch.mean(res).item())
+    print(torch.max(res).item(), '\n')
+
+if __name__ == "__main__":
+    test_model_input_plot()
--- test.py
+++ test.py
@@ -1,27 +1,130 @@
-def test_model_input_plot():
-    import config
+import numpy as np
+import time
+
+def nms_cpu(boxes, confs, nms_thresh=0.5, min_mode=False):
+    # print(boxes.shape)
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    areas = (x2 - x1) * (y2 - y1)
+    order = confs.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        idx_self = order[0]
+        idx_other = order[1:]
+
+        keep.append(idx_self)
+
+        xx1 = np.maximum(x1[idx_self], x1[idx_other])
+        yy1 = np.maximum(y1[idx_self], y1[idx_other])
+        xx2 = np.minimum(x2[idx_self], x2[idx_other])
+        yy2 = np.minimum(y2[idx_self], y2[idx_other])
+
+        w = np.maximum(0.0, xx2 - xx1)
+        h = np.maximum(0.0, yy2 - yy1)
+        inter = w * h
+
+        if min_mode:
+            over = inter / np.minimum(areas[order[0]], areas[order[1:]])
+        else:
+            over = inter / (areas[order[0]] + areas[order[1:]] - inter)
+
+        inds = np.where(over <= nms_thresh)[0]
+        order = order[inds + 1]
+    
+    return np.array(keep)
+
+
+
+def post_processing(img, conf_thresh, nms_thresh, output):
+
+    # anchors = [12, 16, 19, 36, 40, 28, 36, 75, 76, 55, 72, 146, 142, 110, 192, 243, 459, 401]
+    # num_anchors = 9
+    # anchor_masks = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    # strides = [8, 16, 32]
+    # anchor_step = len(anchors) // num_anchors
+
+    # [batch, num, 1, 4]
+    box_array = output[0]
+    # [batch, num, num_classes]
+    confs = output[1]
+
+    t1 = time.time()
+
+    if type(box_array).__name__ != 'ndarray':
+        box_array = box_array.cpu().detach().numpy()
+        confs = confs.cpu().detach().numpy()
+
+    num_classes = confs.shape[2]
+
+    # [batch, num, 4]
+    box_array = box_array[:, :, 0]
+
+    # [batch, num, num_classes] --> [batch, num]
+    max_conf = np.max(confs, axis=2)
+    max_id = np.argmax(confs, axis=2)
+
+    t2 = time.time()
+
+    bboxes_batch = []
+    for i in range(box_array.shape[0]):
+       
+        argwhere = max_conf[i] > conf_thresh
+        l_box_array = box_array[i, argwhere, :]
+        l_max_conf = max_conf[i, argwhere]
+        l_max_id = max_id[i, argwhere]
+
+        bboxes = []
+        # nms for each class
+        for j in range(num_classes):
+
+            cls_argwhere = l_max_id == j
+            ll_box_array = l_box_array[cls_argwhere, :]
+            ll_max_conf = l_max_conf[cls_argwhere]
+            ll_max_id = l_max_id[cls_argwhere]
+
+            keep = nms_cpu(ll_box_array, ll_max_conf, nms_thresh)
+            
+            if (keep.size > 0):
+                ll_box_array = ll_box_array[keep, :]
+                ll_max_conf = ll_max_conf[keep]
+                ll_max_id = ll_max_id[keep]
+
+                for k in range(ll_box_array.shape[0]):
+                    bboxes.append([ll_box_array[k, 0], ll_box_array[k, 1], ll_box_array[k, 2], ll_box_array[k, 3], ll_max_conf[k], ll_max_conf[k], ll_max_id[k]])
+        
+        bboxes_batch.append(bboxes)
+
+    t3 = time.time()
+
+    print('-----------------------------------')
+    print('       max and argmax : %f' % (t2 - t1))
+    print('                  nms : %f' % (t3 - t2))
+    print('Post processing total : %f' % (t3 - t1))
+    print('-----------------------------------')
+    
+    return bboxes_batch
+
+
+def test_yolo_model():
     import torch
-    from yolodwg import DwgKeyPointsModel, DwgKeyPointsResNet50, non_zero_loss
-    from plot import plot_batch_grid
+    from models.DwgKeyPointsYolov4 import DwgKeyPointsYolov4
+    import config
 
-    #model = DwgKeyPointsModel(max_points=max_points, num_coordinates=num_coordinates).to(config.device)
-
-    checkpoint = torch.load('runs/2/best.weights', map_location=config.device)
-    max_points = checkpoint['max_points']
-    num_pnt_classes = checkpoint['num_pnt_classes']
-
-    model = DwgKeyPointsModel(max_points=max_points, num_pnt_classes=num_pnt_classes)
-    #model = DwgKeyPointsResNet50(requires_grad=True, max_points=max_points)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    model.to(config.device)
-    model.eval()
+    model = DwgKeyPointsYolov4(max_points=30)
+    model.train()
 
     res = None
 
     for i in range(100):
-        imgs = torch.rand([4, 3, 128, 128])
-        imgs = imgs.to(config.device)
+        imgs = torch.rand([8, 3, 128, 128]).to(config.device)
         out = model(imgs)
+        #output shape [batch * 1008 * 4(xyhw), batch*1008*n_classes]
+        bboxes = post_processing(img=None, conf_thresh=0.1,nms_thresh=0.5, output=out)
+
         if res is None:
             res = out
         else:
@@ -36,5 +139,7 @@ def test_model_input_plot():
     print(torch.mean(res).item())
     print(torch.max(res).item(), '\n')
 
+
+
 if __name__ == "__main__":
-    test_model_input_plot()
+    test_yolo_model()
